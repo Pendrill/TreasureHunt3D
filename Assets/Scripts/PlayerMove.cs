5bebbbd3d5189cd449ed4cdee4bd97d1d82053a2
Aleksei Waddington <Aleksei Waddington>
2016-09-17 15:27:10 -0400

    Camera will now follow the player character

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -9,7 +9,7 @@ public class PlayerMove : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		//We set the offset to 0 at the start
-		CameraOffSet = new Vector3 (0, 0, 0);
+		//CameraOffSet = new Vector3 (0, 0, 0);
 		lastButtonPressed = 'w';
 	}
 	
@@ -87,6 +87,8 @@ public class PlayerMove : MonoBehaviour {
 			transform.Translate (0,0,10*Time.deltaTime);
 			lastButtonPressed = 's';
 		}
+
+		Camera.main.transform.position = transform.position + CameraOffSet;
 		//The player can use the E and Q or Delete and Left Shift to rotate the player (ie. change where they are facing)
 		/*if (Input.GetKey (KeyCode.E) || Input.GetKey (KeyCode.PageDown)) {
 			transform.eulerAngles += new Vector3 (0, 5, 0);
