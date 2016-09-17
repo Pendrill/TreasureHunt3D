57dcdbae8adfc6a23354b606fea373e580e5b568
Aleksei Waddington <Aleksei Waddington>
2016-09-17 12:44:40 -0400

    Added more comments to the move script

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,39 +2,37 @@
 using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
-	public Vector3 playerPosition;
+	//We set up a Vector3 to take care of the CameraOffSet
 	public Vector3 CameraOffSet;
 	//public bool isForward=true;
 	// Use this for initialization
 	void Start () {
-		playerPosition = transform.position;
+		//We set the offset to 0 at the start
 		CameraOffSet = new Vector3 (0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//The player can use WASD and the Arrow Keys to more the player character around
 		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
-			//playerPosition += new Vector3 (10, 0, 0) * Time.deltaTime; 
+			//The use of transform.Translate will affect the player movement at a local scale rather than a global one
 			transform.Translate (10f * Time.deltaTime, 0f,0f);
 		}
 		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
-			//playerPosition += new Vector3 (-10, 0, 0) * Time.deltaTime; 
 			transform.Translate (-10f * Time.deltaTime, 0f,0f);
 		}
-		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
-			//playerPosition += new Vector3 (0, 0, 10) * Time.deltaTime; 
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) { 
 			transform.Translate (0,0,10*Time.deltaTime);
 		}
 		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
-			//playerPosition += new Vector3 (0, 0, -10) * Time.deltaTime;
 			transform.Translate (0,0,-10*Time.deltaTime);
 		}
+		//The player can use the E and Q or Delete and Left Shift to rotate the player (ie. change where they are facing)
 		if (Input.GetKey (KeyCode.E) || Input.GetKey (KeyCode.PageDown)) {
 			transform.eulerAngles += new Vector3 (0, 5, 0);
 		}
 		if (Input.GetKey (KeyCode.Q) || Input.GetKey (KeyCode.RightShift)) {
 			transform.eulerAngles += new Vector3 (0, -5, 0);
 		}
-		//transform.position = playerPosition;
 	}
 }
