b3d7c9840868dfb5426b151a260b91f34b6c12b8
Aleksei Waddington <Aleksei Waddington>
2016-09-17 16:12:57 -0400

    Added a treasure object that can be obtained by pressing [SPACE]. Text appears as you approach the item and changes when the treasure is picked up.

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+
+	public bool didPlayerWin = false;
+	public Text objectText;
+	public Transform playerPosition;
+	public Transform treasurePosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (didPlayerWin) {
+			objectText.text = "YOU GOT TREASURE AND YOU WIN";
+		} else if ((playerPosition.position - treasurePosition.position).magnitude < 5f) {
+			objectText.text = "Press [SPACE] to get treasure!";
+			if (Input.GetKey (KeyCode.Space)) {
+				didPlayerWin = true;
+			}
+		} else {
+			objectText.text =  "";
+		}
+	}
+}
