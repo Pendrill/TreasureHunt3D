4e2fa558472860805f05005820061e7f69ecfec1
Aleksei Waddington <Aleksei Waddington>
2016-09-18 22:24:39 -0400

    Set up all the text for the objects. Updated the code to work with every interactable object.

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -5,10 +5,13 @@ using UnityEngine.UI;
 public class GameLogic : MonoBehaviour {
 
 	public bool didPlayerWin = false;
+	public bool isDoor = false;
+	public float maxMagnitude;
 	public Text objectText;
 	public Transform playerPosition;
 	public Transform treasurePosition;
-
+	public string objectMessage = "";
+	public GameObject panel;
 	// Use this for initialization
 	void Start () {
 	
@@ -17,11 +20,17 @@ public class GameLogic : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (didPlayerWin) {
-			objectText.text = "YOU GOT TREASURE AND YOU WIN";
-		} else if ((playerPosition.position - treasurePosition.position).magnitude < 5f) {
-			objectText.text = "Press [SPACE] to get treasure!";
-			if (Input.GetKey (KeyCode.Space)) {
-				didPlayerWin = true;
+			objectText.text = "YOU WENT THROUGH THE DOOR";
+			panel.SetActive (true);
+		} else if ((playerPosition.position - treasurePosition.position).magnitude < maxMagnitude) {
+			if (isDoor) {
+				objectText.text = objectMessage;
+				if (Input.GetKey (KeyCode.Space)) {
+					didPlayerWin = true;
+				}
+			} else {
+				//Debug.Log ("is player by the house?");
+				objectText.text = objectMessage;
 			}
 		} else {
 			objectText.text =  "";
