fcbcc582e39f9877110a2231a13231701477d819
Aleksei Waddington <Aleksei Waddington>
2016-09-19 00:04:52 -0400

    Added comments to the gameLogic.cs code yay comments

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -4,35 +4,48 @@ using UnityEngine.UI;
 
 public class GameLogic : MonoBehaviour {
 
+	//check if the player wins
 	public bool didPlayerWin = false;
+	//check if the object is the treasue/final door
 	public bool isDoor = false;
+	//define how far the player has to be from the object before seeing the directional text
 	public float maxMagnitude;
 	public Text objectText;
 	public Transform playerPosition;
 	public Transform treasurePosition;
+	//define what message is diplayed at one specific pbject
 	public string objectMessage = "";
+	//display a black panel at the end of the game
 	public GameObject panel;
 	// Use this for initialization
 	void Start () {
-	
+		//nothing happens here
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//check if the player won
 		if (didPlayerWin) {
+			//display the winning text
 			objectText.text = "YOU WENT THROUGH THE DOOR";
+			//activate a black panel to seperate the end from the rest of the game
 			panel.SetActive (true);
+		//Check where the player is located in relation to the object.
 		} else if ((playerPosition.position - treasurePosition.position).magnitude < maxMagnitude) {
+			//check if the player is in front of the final game.
 			if (isDoor) {
 				objectText.text = objectMessage;
+				//let the player press space to go through the door
 				if (Input.GetKey (KeyCode.Space)) {
 					didPlayerWin = true;
 				}
+			//the player is next to an object that is not the final door.
 			} else {
 				//Debug.Log ("is player by the house?");
 				objectText.text = objectMessage;
 			}
 		} else {
+			//The player is not next to any object and thus no text is displayed.
 			objectText.text =  "";
 		}
 	}
